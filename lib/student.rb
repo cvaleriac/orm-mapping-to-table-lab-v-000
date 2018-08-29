9588cc50f756ab4d4f8bd1aa59a72124f89ff0a6
valeria campilongo <cvaleriac@hotmail.it>
2018-08-29 22:42:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,4 +33,11 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  
+  def self.create(name:, grade:)
+    song = Student.new(name, grade)
+    student.save
+    student
+  end
+  
 end
